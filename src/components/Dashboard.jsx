614d091e63874a3d941b0eeee1b170836ed79454
ShiCho489 <choudhary.shivani489@gmail.com>
2023-09-21 09:05:53 +0300

    dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { auth } from "../firebase";
 import NavBar from "../public/NavBar";
 //import { signOut } from "firebase/auth";
-import Hoistone from '../components/Hoistone'
+
 
 import Sidebar from '../components/Sidebar';
 
@@ -15,17 +15,15 @@ const Dashboard = () => {
     return(
         
           <div className = "container">
+            <NavBar />
 
-<NavBar />
-
-<div className = "row justify-content-center">
-   <div className = "col-md-4 text-center">
-  
-   <p className = "lead">Welcome to Konecranes Crane Portal!</p>
+        <div className = "row justify-content-center">
+        <div className = "col-md-4 text-center">
+        <p className = "lead">Welcome to Konecranes Crane Portal!</p>
        <p>Welcome <em className = "text-decoration-underline">{ user.email }</em>. Follow the dashboard to track and monitor your hoists with ease. We're here to make your hosting operations more efficient and convinient.</p>
        <Sidebar />
     <div className = "d-grid gap-2">
-          <Hoistone />
+      
 
        </div>                
    </div>
---
+++ src/components/Hoist.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Hoist = () => {
+ 
+  return (
+    <div>
+    <h1>Hoist Control</h1>
+     </div>
+  )
+}
+
+export default Hoist
