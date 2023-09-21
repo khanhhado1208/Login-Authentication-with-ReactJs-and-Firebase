06ed2f05272361de35b35aa56012f59247ce2414
unknown <khanhhado1208@gmail.com>
2023-09-21 12:40:31 +0300

    fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,5 @@
 import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth"
-//import { getFirestore } from "firebase/firestore";
-import firebase from 'firebase/app';
-import 'firebase/database';
-
-
-
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyAodniW578JZYe5UfqjpYgLfCHExB093MU",
@@ -19,15 +12,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
-export {auth}
 
-const database = firebase.database();
-
-export {app, database};
-//export default firebase;
-
-
-//export const db = getFirestore(app)
-
-
-
+export {auth}
