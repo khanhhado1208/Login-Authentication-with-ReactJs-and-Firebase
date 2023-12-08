e5feb3bf76dd04646873b433560d882c399264a0
Shivani <choudhary.shivani489@gmail.com>
2023-12-08 10:47:56 +0200

    formatted the timer

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { auth } from "../firebase";
 import NavBar from "../public/NavBar";
-import { writeValue } from '../firestore';
+import { writeValue, readValue } from '../firestore';
 
 const Dashboard = () => {
   const user = auth.currentUser;
   const [selectedMode, setSelectedMode] = useState('control');
   const [isOn, setIsOn] = useState(false);
-  const [count, setCount] = useState(0);
+  //const [count, setCount] = useState(0);
+  const [count, setCount] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  }); 
+
+  useEffect(() => {
+    const fetchCount = async () => {
+        const timerValue = await readValue();
+        setCount(timerValue);
+    };
+    fetchCount();
+  }, []);
 
   const startTimer = () => {
     setIsOn(true);
@@ -47,13 +61,17 @@ const Dashboard = () => {
     }
   };
 
-  const formatTime = (time) => {
-    const hours = Math.floor(time / 3600);
-    const minutes = Math.floor((time % 3600) / 60);
-    const seconds = time % 60;
+  const formatTime = (count) => {
+    
+    const days = Math.floor(count /(3600 * 24));
+    const hours = Math.floor(count / 3600);
+    const minutes = Math.floor((count % 3600) / 60);
+    const seconds = count % 60;
 
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    return `${String(days).padStart(2, '0')}:${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
   };
+
+  const formattedTime = formatTime(count);
 
   return (
     <>
@@ -70,19 +88,23 @@ const Dashboard = () => {
             </p>
             <div className="card p-4 py-4">
               <h4 className="mb-3"><small>Content for Remote Control Mode</small></h4>
-              <p className="fs-2 mb-4">Timer Value: {formatTime(count)}</p>
+              {
+                
+               <p className="fs-2 mb-4">Timer Value: {formattedTime}</p>
+              }
+              
               <div className="d-flex justify-content-center align-items-center">
                 <button
                   className={`btn btn-lg ${isOn ? 'btn-danger' : 'btn-success'} me-2`}
                   onClick={toggleOnOff}
-                  style={{ width: "48%" }}
+                  style={{ width: "20%" }}
                 >
                   {isOn ? 'Off' : 'On'}
                 </button>
                 <button
                   className="btn btn-lg btn-warning"
                   onClick={resetTimer}
-                  style={{ width: "48%" }}
+                  style={{ width: "20%" }}
                 >
                   Reset
                 </button>
--- src/firestore.js
+++ src/firestore.js
@@ -3,14 +3,26 @@ import { db } from './firebase';
 
 export const readValue = async ()=> {
     try {
-        const timerRef = doc(db, "Hoist", "timer")
+        const timerRef = doc(db, "Hoist", "Timer")
         const docSnap = await getDoc(timerRef);
          
 
-        if (docSnap.exists()){
-           return docSnap.data().count;
+        if (docSnap.exists()) {
+          const timerValue = docSnap.data();
+          const { days, hours, minutes, seconds} = timerValue;
+          return {
+            days:days,
+            hours:hours,
+            minutes: minutes,
+            seconds: seconds,
+           };
         } else {
-            return 0;
+            return {
+              days: 0,
+              hours: 0,
+              minutes: 0,
+              seconds: 0,
+            } 
         }
     } catch (error) {
 
@@ -18,18 +30,27 @@ export const readValue = async ()=> {
 
 }
 
-
-
-
 export const writeValue = async (count) => {
  
-const timerRef = doc(db, 'Hoist', 'timer');
+const timerRef = doc(db, 'Hoist', 'Timer');
+const days = Math.floor(count /(3600 * 24));
+const hours = Math.floor(count / 3600);
+const minutes = Math.floor((count % 3600) / 60);
+const seconds = count % 60;
 
 try {
-    await setDoc(timerRef, { count: count });
+    await setDoc(timerRef, { 
+      count: count,
+      days:days,
+      hours:hours,
+      minutes:minutes,
+      seconds:seconds
+    
+    });
     console.log("Timer value updated in Firestore");
   } catch (error) {
     console.error("Error updating timer value:", error);
   }
 
 }
+
